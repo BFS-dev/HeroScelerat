3c9d8b9d6e2cb49384a47cb0948dd7ce9b6d1c41
samurollie <53665491+samurollie@users.noreply.github.com>
2021-01-28 21:51:00 -0300

    Movimentação

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,9 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Update is called once per frame
+    public Animator animator;
+    public Joystick joystick;
+    float inputX = 0;
+    float inputY = 0;
+    public float speed = 2.5f;
+    bool isWalking = false;
+
+    void Start() {
+        isWalking = false;
+    }
+
     void Update()
     {
-        if(Input.touchCount > 0)
+        inputX = joystick.Horizontal;
+        inputY = joystick.Vertical;
+        isWalking = (inputX != 0 || inputY != 0) ? true : false;
+
+        if (isWalking) {
+            var move = new Vector3(inputX, inputY, 0).normalized;
+            transform.position += move * speed * Time.deltaTime;
+            animator.SetFloat("posX", inputX);
+            animator.SetFloat("posY", inputY);
+        }
+
+        animator.SetBool("isWalking", isWalking);
+        /* if(Input.touchCount > 0)
         {
             Touch t = Input.GetTouch(0);
 
@@ -26,7 +49,7 @@ public class PlayerController : MonoBehaviour
                 {
                     transform.position += Vector3.up * 1;
                 }
-            } */
-        }
+            } 
+        }*/
     }
 }
