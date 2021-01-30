b541af63fcacd25414467bf0fb7e68f702a79460
Bruna Torres <blts@ic.ufal.br>
2021-01-29 23:24:03 -0300

    Implemetando caixinha do diálogo.

---
+++ Assets/Scripts/Diálogo/TypeWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class TypeWriter : MonoBehaviour {
+
+    public Text textWriter;
+    public float delayWriter = 0.1f;
+    public string escrevaFrase;
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        StartCoroutine("mostrar_text", escrevaFrase);
+    }
+
+    IEnumerator mostrar_text( string textType ) 
+    {
+        textWriter.text = ""; // auxiliar que começa vazia e vai sendo preenchida letra por letra.
+
+        for(int letter = 0; letter < textType.Length; letter++)
+        {
+            textWriter.text = textWriter.text + textType[letter];
+            yield return new WaitForSeconds(delayWriter);
+        }
+
+    }
+}
