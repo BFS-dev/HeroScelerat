44a377cea2825f382196da8fba7b8afacac27513
Bruna Torres <blts@ic.ufal.br>
2021-02-02 16:25:57 -0300

    Falando com Clarice

---
+++ Assets/Scripts/Diálogo/ComClarice.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ComClarice : MonoBehaviour
+{
+    [SerializeField]
+    private GerenciadorDialogo gerenciador;
+
+    [SerializeField]
+    private Dialogo dialogo;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Clarice")){
+            Debug.Log("Colidiu com Clarice");
+            gerenciador.Inicializa(dialogo);
+        }
+        
+    }
+}
